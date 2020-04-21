1651043d3683ab626c6d16d87c78df6811db4d07
Hiromu Sugiura <h.sugiura0705@outlook.com>
2020-04-21 13:34:44 +0900

    error handling

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,8 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn latex_to_mathml(input: String) -> String {
-    latex2mathml::latex_to_mathml(&input, latex2mathml::DisplayStyle::Block).unwrap()
+    match latex2mathml::latex_to_mathml(&input, latex2mathml::DisplayStyle::Block) {
+        Ok(mathml) => mathml,
+        Err(e)     => format!("[ERROR] {}", e),
+    }
 }
